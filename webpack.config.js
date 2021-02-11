ca03240e0994bc34c414e3f411731cd798b3bf9a
Tatiana Frank <yunikiro@gmail.com>
2021-02-11 19:38:39 +0300

    Added configuration for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,10 @@ module.exports = {
                 }
             }, {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource'
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
             }
         ]
     }, 
